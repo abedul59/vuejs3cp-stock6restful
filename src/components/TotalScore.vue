939fd8fe4b22ab241d557abefcf411517ae73f57
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-13 12:22:04 +0800

    4/13

--- src/components/TotalScore.vue
+++ src/components/TotalScore.vue
@@ -25,7 +25,7 @@
       </td>
       <td align="center">
         <div>
-          <FONT SIZE="7">{{ cStockName }}</FONT>
+          <FONT SIZE="7">{{ props.cStockName }}</FONT>
         </div>
       </td>
     </tr>
@@ -121,7 +121,7 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   cStockName: String,
   cNewestSeason: String
 })
