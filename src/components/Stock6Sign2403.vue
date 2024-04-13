7c8a0461ed535dd75987bbb2c7b039089623a6fb
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-13 08:50:07 +0800

    4/13

--- src/components/Stock6Sign2403.vue
+++ src/components/Stock6Sign2403.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref, toRefs, reactive } from 'vue'
+import { ref, toRefs, reactive, unRef } from 'vue'
 
 const inputText2 = ref('測試!')
 const result2 = ref('')
 const stock6data2 = reactive({})
-const cStockName = ref('')
+const c2StockName = ref('')
 console.log(stock6data2)
 
 defineProps({
@@ -22,10 +22,10 @@ function handleSubmit() {
     .then((data) => data.json())
     .then((response) => {
       const { cStockName, cNewestSeason } = toRefs(response)
-      cStockName.value = cStockName
+      c2StockName.value = unRef(cStockName)
       const stock6data2 = reactive(response)
 
-      console.log(cStockName.value)
+      console.log(c2StockName.value)
       console.log(cNewestSeason.value)
       console.log(stock6data2)
     })
@@ -39,7 +39,7 @@ function handleSubmit() {
     <div v-if="result2">
       <p>Result2: {{ result2 }}</p>
 
-      <p>股票名稱: {{ cStockName }}</p>
+      <p>股票名稱: {{ c2StockName }}</p>
     </div>
   </div>
   <br />
